e70ea128d1d91d323e985c3ca2457fd65eea4a6e
Paul Opiyo <59094545+paulopiyo777@users.noreply.github.com>
2020-06-27 15:47:52 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// setup express server
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API running"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
